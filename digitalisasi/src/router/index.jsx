97e15d6442e67e5216ea720133d3db4c38cd28b4
ffznrr <ffznrr@gmail.com>
2025-05-06 16:16:41 +0700

    fix: fixing routing

--- digitalisasi/src/router/index.jsx
+++ digitalisasi/src/router/index.jsx
@@ -1,21 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../view/InputForm";
-import BaseLayout from "../components/baselayout";
-import ListCustomer from "../view/listcustomer";
 
 const router = createBrowserRouter([
   {
-    element: <BaseLayout />,
-    children: [
-      {
-        path: "/",
-        element: <App />,
-      },
-      {
-        path: "/listcustomer",
-        element: <ListCustomer />,
-      },
-    ],
+    path: "/",
+    element: <App />,
   },
 ]);
 
--- digitalisasi/src/view/InputForm.jsx
+++ digitalisasi/src/view/InputForm.jsx
@@ -26,74 +26,76 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1 className="text-center">Form Input Kustomer Kredit</h1>
-      <form onSubmit={handleSubmit} className="p-24">
-        <div>
-          <label htmlFor="nama">Nama:</label>
-          <input
-            className="border-2 border-black w-full mb-5"
-            type="text"
-            id="nama"
-            value={nama}
-            onChange={(e) => setNama(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="umur">Umur:</label>
-          <input
-            className="border-2 border-black w-full mb-5"
-            type="number"
-            id="umur"
-            value={umur}
-            onChange={(e) => setUmur(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="alamat">Alamat:</label>
-          <textarea
-            className="border-2 border-black w-full mb-5"
-            id="alamat"
-            value={alamat}
-            onChange={(e) => setAlamat(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="tenor">Tenor (bulan):</label>
-          <input
-            className="border-2 border-black w-full mb-5"
-            type="number"
-            id="tenor"
-            value={tenor}
-            onChange={(e) => setTenor(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="pekerjaan">Pekerjaan:</label>
-          <input
-            className="border-2 border-black w-full mb-5"
-            type="text"
-            id="pekerjaan"
-            value={pekerjaan}
-            onChange={(e) => setPekerjaan(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="HP">Nomor HP:</label>
-          <input
-            className="border-2 border-black w-full mb-5"
-            type="text"
-            id="HP"
-            value={HP}
-            onChange={(e) => setHP(e.target.value)}
-          />
-        </div>
-        <div className="flex justify-center">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+    <div className="min-w-screen min-h-screen">
+      <div className="w-full ">
+        <h1 className="text-center">Form Input Kustomer Kredit</h1>
+        <form onSubmit={handleSubmit} className="p-24">
+          <div>
+            <label htmlFor="nama">Nama:</label>
+            <input
+              className="border-2 border-black w-full mb-5"
+              type="text"
+              id="nama"
+              value={nama}
+              onChange={(e) => setNama(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="umur">Umur:</label>
+            <input
+              className="border-2 border-black w-full mb-5"
+              type="number"
+              id="umur"
+              value={umur}
+              onChange={(e) => setUmur(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="alamat">Alamat:</label>
+            <textarea
+              className="border-2 border-black w-full mb-5"
+              id="alamat"
+              value={alamat}
+              onChange={(e) => setAlamat(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="tenor">Tenor (bulan):</label>
+            <input
+              className="border-2 border-black w-full mb-5"
+              type="number"
+              id="tenor"
+              value={tenor}
+              onChange={(e) => setTenor(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="pekerjaan">Pekerjaan:</label>
+            <input
+              className="border-2 border-black w-full mb-5"
+              type="text"
+              id="pekerjaan"
+              value={pekerjaan}
+              onChange={(e) => setPekerjaan(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="HP">Nomor HP:</label>
+            <input
+              className="border-2 border-black w-full mb-5"
+              type="text"
+              id="HP"
+              value={HP}
+              onChange={(e) => setHP(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
 
-      <ListCustomer customers={customers} />
+        <ListCustomer customers={customers} />
+      </div>
     </div>
   );
 }
